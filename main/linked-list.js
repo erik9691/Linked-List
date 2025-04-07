3f4e0c6a1257b93d50a894aec24ca09dcccfefdf
erik9691 <gagwaffle@gmail.com>
2025-04-06 21:49:09 -0300

    Added more functions and refactored to not use arrays
    Added prepend, size, head, tail, at, pop and contains

--- main/linked-list.js
+++ main/linked-list.js
@@ -1,17 +1,85 @@
 class LinkedList {
 	constructor() {}
 
-	nodes = [];
+	headNode = null;
+	tailNode = null;
+	length = 0;
 
 	append(value) {
 		const newNode = new Node(value);
 
-		if (this.nodes.length > 0) {
-			const prevNode = this.nodes.slice(-1)[0];
-			prevNode.nextNode = newNode;
+		if (this.headNode === null) {
+			this.headNode = newNode;
+			this.tailNode = newNode;
+		} else {
+			this.tailNode.nextNode = newNode;
+			this.tailNode = newNode;
 		}
 
-		this.nodes.push(newNode);
+		this.length += 1;
+	}
+	prepend(value) {
+		const newNode = new Node(value);
+
+		if (this.headNode === null) {
+			this.headNode = newNode;
+			this.tailNode = newNode;
+		} else {
+			newNode.nextNode = this.headNode;
+			this.headNode = newNode;
+		}
+
+		this.length += 1;
+	}
+	size() {
+		return this.length;
+	}
+	head() {
+		return this.headNode;
+	}
+	tail() {
+		return this.tailNode;
+	}
+	at(index) {
+		if (index < 0) {
+			throw new Error("ERROR index must be 0 or higher");
+		} else if (index + 1 > this.length) {
+			throw new Error("ERROR index can't be larger than list");
+		} else if (index === 0) {
+			return this.headNode;
+		} else if (index + 1 === this.length) {
+			return this.tailNode;
+		} else {
+			let currentNode = this.headNode;
+			for (let i = 0; i < index; i++) {
+				currentNode = currentNode.nextNode;
+			}
+			return currentNode;
+		}
+	}
+	pop() {
+		if (this.length === 1) {
+			this.headNode = null;
+			this.tailNode = null;
+			this.length -= 1;
+		} else if (this.length === 0) {
+			throw new Error("Can't pop an empty list");
+		} else {
+			const secondToLastNode = this.at(this.length - 2);
+			secondToLastNode.nextNode = null;
+			this.tailNode = secondToLastNode;
+			this.length -= 1;
+		}
+	}
+	contains(value) {
+		let currentNode = this.headNode;
+		for (let i = 0; i < this.length; i++) {
+			if (currentNode.value === value) {
+				return true;
+			}
+			currentNode = currentNode.nextNode;
+		}
+		return false;
 	}
 }
 
@@ -28,4 +96,5 @@ const list = new LinkedList();
 
 list.append("dude");
 list.append("bro");
-console.log(list.nodes[0].nextNode);
+list.prepend("yes");
+list.pop();
